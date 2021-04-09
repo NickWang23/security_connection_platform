1e37e80d1acfeaa7c0c5fbe7546a8f06ce36eb00
NickWang23 <31427742+NickWang23@users.noreply.github.com>
2021-04-09 14:55:34 +0800

    Delete test directory

--- test/API_test/__init__.py
+++
@@ -1,7 +0,0 @@
-# !/usr/bin/env python
-# -*-coding: utf-8-*-
-
-# @ProjectName : safe_interface
-# @FileName : __init__.py.py
-# @Time : 2019/12/25 16:55
-# @Author : Nick
--- test/API_test_v2/conftest.py
+++
@@ -1,9 +0,0 @@
-# !/usr/bin/env python
-# -*-coding: utf-8-*-
-
-# @ProjectName : security_connection_platform(SCP)
-# @FileName : conftest.py.py
-# @Time : 2020/3/17 9:09
-# @Author : Nick
-
-
